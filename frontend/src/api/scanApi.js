d055dbabe08cdf231e41a7eaa1d06d929b1f7e99
StanyGratus <staanygratus2006@gmail.com>
2026-06-25 11:23:41 +0530

    Modifed .env

--- frontend/src/api/scanApi.js
+++ frontend/src/api/scanApi.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 
 // Create React App reads variables from the environment automatically using process.env.REACT_APP_
 const API_BASE_URL = process.env.REACT_APP_RENDER_URL;
+console.log("API URL:", API_BASE_URL);
 
 export const runScan = async (target) => {
   const response = await axios.post(`${API_BASE_URL}/scan`, { target });
   return response.data;
 };
+
 
 export const fetchHistory = async () => {
   const response = await axios.get(`${API_BASE_URL}/scan/history`);
