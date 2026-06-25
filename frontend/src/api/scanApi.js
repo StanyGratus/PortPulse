2a844230bfc5cf040f32a1ed799d72db21bbd6af
StanyGratus <staanygratus2006@gmail.com>
2026-06-25 10:43:30 +0530

    Added env info

--- frontend/src/api/scanApi.js
+++ frontend/src/api/scanApi.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 require('dotenv').config();
 
 export const runScan = async (target) => {
-  const response = await axios.post(`${process.env.URL}/scan`, { target });
+  const response = await axios.post(`${process.env.FRONTEND_URL}/scan`, { target });
   return response.data;
 };
 
 export const fetchHistory = async () => {
-  const response = await axios.get(`${process.env.URL}/scan/history`);
+  const response = await axios.get(`${process.env.FRONTEND_URL}/scan/history`);
   return response.data;
 };
 
 export const clearHistory = async () => {
-  const response = await axios.delete(`${process.env.URL}/scan/history`);
+  const response = await axios.delete(`${process.env.FRONTEND_URL}/scan/history`);
   return response.data;
 };
