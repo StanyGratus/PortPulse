0cd1df6178f571153899ffb99f82ce77f94a2ff5
StanyGratus <staanygratus2006@gmail.com>
2026-06-25 07:36:38 +0530

    production backend URL

--- frontend/src/api/scanApi.js
+++ frontend/src/api/scanApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://portpulse.onrender.com/api';
 
 export const runScan = async (target) => {
   const response = await axios.post(`${BASE_URL}/scan`, { target });
